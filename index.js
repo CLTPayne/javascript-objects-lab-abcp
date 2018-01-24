b2460a7e7c8200c6fe6ce152c5cd0c9e9572c859
Charlotte Payne <cltpayne@gmail.com>
2018-01-24 00:21:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,3 @@ function updateObjectWithKeyAndValue(object, key, value) {
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
 }
-
-function cape() {
-  var obj = { prop: 1 }
-  obj.prop2 = 2
-  return obj
-}
