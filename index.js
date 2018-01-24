14496fe4dd838428896cb9e82797b71745e23b61
Charlotte Payne <cltpayne@gmail.com>
2018-01-24 00:28:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key, value) {
-  const newObj = Object.assign({}, obj)
-  return object
+  const newObj = Object.assign({}, object)
+  delete newObj.key
+  return newObj
 }
